56ec82cb80dddc28c6ce920f26c611b1363919c1
Cooper95 <cooper.rehn@att.net>
2014-04-02 09:57:41 -0500

    Update Pancake.cpp

--- Pancake.cpp
+++ Pancake.cpp
@@ -4,5 +4,9 @@
 
 #include "Pancake.h"
 
-Pancake::Pancake(int size){
-  
+Pancake::Pancake(int size, int position){
+  int width = 40 + 40 * size;
+  int height = 20;
+  Graph_lib::Point point = Graph_lib::Point(FlipFlap::X_CENTER - width/2, FlipFlap::TABLE_TOP + height * position)
+  Rectangle(point, width, height);
+}
