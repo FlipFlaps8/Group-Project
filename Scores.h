bcef550dd3a04391f1214df20eaaf4b10a7c0753
drewmitchell94 <drew.mitchell94@yahoo.com>
2014-04-02 14:43:59 -0500

    Update Scores.h

--- Scores.h
+++ Scores.h
@@ -3,5 +3,23 @@
 //Scores.h
 
 void find_solution();
-void get_top5();
-void write_top5();
+
+void write_top5(Player& score){
+  ofstream oscore_file;
+  oscore_file.open("HighScore.txt");
+  oscore_file<<scorel;
+  oscore_file.close;
+  
+}
+struct Player { //contains player name and score
+  string name;
+  int score;
+}
+
+Player scores[5];
+
+void get_top5(){
+  ifstream iscore_file("HighScore.txt")
+  iscore_file>>
+  
+}
