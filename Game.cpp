9a8d7b64eb33834d542e3f85e0b69bf37e6a4a5c
Cooper95 <cooper.rehn@att.net>
2014-04-02 18:41:15 -0500

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -4,9 +4,12 @@
 
 #include "Game.h"
 #include "FlipFlap.h"
+#include "Pancake.h"
 
 void Game::setup(int level){
   //draw pancakes
+  vector<Pancake::Pancake> pancake_stack(level);
+  
   //draw buttons
   //display score
 }
