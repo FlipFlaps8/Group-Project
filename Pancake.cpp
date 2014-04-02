b609334ea88dcf66eb8c2c42a71c68b9ebc27a8e
Cooper95 <cooper.rehn@att.net>
2014-04-02 16:35:52 -0500

    Update Pancake.cpp

--- Pancake.cpp
+++ Pancake.cpp
@@ -4,9 +4,16 @@
 
 #include "Pancake.h"
 
-Pancake::Pancake(int size, int position){
-  int width = 40 + 40 * size;
-  int height = 20;
-  Point point = Point(FlipFlap::X_CENTER - width/2, FlipFlap::TABLE_TOP + height * position)
-  Rectangle(point, width, height);
+int height = 20;
+
+int width1(int s1){
+  return 40 + 40 * s1;
 }
+
+Point pPoint(int s, int p){
+        return Point(FlipFlap::X_CENTER - width1(s)/2, FlipFlap::TABLE_TOP - height * p);
+}
+
+Pancake::Pancake(int size, int position) : Rectangle(pPoint(size,position),width1(size),20), s(size), p(position){}
+
+Pancake::Pancake(const Pancake& p1) : Rectangle(pPoint(p1.s, p1.p),width1(p1.s),20), s(p1.s), p(p1.p){}
