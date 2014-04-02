5bdf681c0728a98c69e0c1bb61b53c063c6993c7
Cooper95 <cooper.rehn@att.net>
2014-04-02 09:31:40 -0500

    Create FlipFlap.cpp

---
+++ FlipFlap.cpp
@@ -0,0 +1,29 @@
+//Group 8
+//CSCE 121-501
+//FlipFlap.cpp
+
+#include "FlipFlap.h"
+
+bool FlipFlap::is_running(){
+  return running;
+}
+
+int current_screen(){
+  return screen;
+}
+
+void FlipFlap::show_splash(){
+
+}
+
+void FlipFlap::show_levels(){
+
+}
+
+void FlipFlap::show_game(){
+
+}
+
+void FlipFlap::show_score(){
+
+}
