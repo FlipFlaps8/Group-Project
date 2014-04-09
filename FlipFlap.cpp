0cecae8f35be28d02bcbca8fc265b9eb162b3ed9
Cooper95 <cooper.rehn@att.net>
2014-04-09 09:55:26 -0500

    Update FlipFlap.cpp

--- FlipFlap.cpp
+++ FlipFlap.cpp
@@ -3,10 +3,8 @@
 //FlipFlap.cpp
 
 #include "FlipFlap.h"
-#include "Game.h"
+#include "Pancake.h"
 //add whatever includes you need to make your show function work
-
-FlipFlap::FlipFlap() : running(true), screen(FlipFlap::SPLASH_SCREEN){}
 
 bool FlipFlap::is_running(){
   return running;
@@ -20,6 +18,32 @@ void FlipFlap::done(){
   running = false;
 }
 
+vector<int> pancake_stack(int l){
+  vector<int> pancakes(l);
+  for(int i =0; i < l; ++i)
+    pancakes[i] = i;
+  random_shuffle(pancakes.begin(), pancakes.end());
+  return pancakes;
+}
+
+void FlipFlap::setup(int level){
+  //draw pancakes
+  vector<int> game_stack = pancake_stack(level);
+  vector<Pancake*> game_stack_p(game_stack.size());
+  for(int i = 0; i < game_stack.size(); ++i){
+    game_stack_p[i] = new Pancake(game_stack[i],i);
+  }
+  for(Pancake* pancake : game_stack_p){
+    win.attach(*pancake);
+  }
+  win.wait_for_button();
+  //draw buttons
+  //display score
+}
+
+void FlipFlap::update(){
+}
+
 void FlipFlap::show_splash(){
 
 }
@@ -29,9 +53,9 @@ void FlipFlap::show_levels(){
 }
 
 void FlipFlap::show_game(){
-  setup();
+  setup(9);
 }
 
-void FlipFlap::show_score(){
+void FlipFlap::show_scores(){
 
 }
