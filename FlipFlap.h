d2d8d641a3fb174e1a61d4b9632cbd0729f74d8b
Cooper95 <cooper.rehn@att.net>
2014-04-09 09:54:40 -0500

    Update FlipFlap.h

--- FlipFlap.h
+++ FlipFlap.h
@@ -2,31 +2,42 @@
 //CSCE 121-501
 //FlipFlap.h
 
+#ifndef FLIPFLAP_GUARD
+#define FLIPFLAP_GUARD
+
 #include "std_lib_facilities_4.h"
 #include "Simple_window.h"
 #include "Graph.h"
 
+using namespace Graph_lib;
+
 struct FlipFlap{
-  
+
   static const int X_MAX = 599;
   static const int Y_MAX = 399;
   static const int X_CENTER = 299;
   static const int Y_CENTER = 199;
   static const int TABLE_TOP = 299;
-  static Simple_window win(Point(100,100),600,400,"FlipFlap"); //Game window
-  
+  Simple_window win;
+
   enum{SPLASH_SCREEN, LEVELS_SCREEN, GAME_SCREEN, SCORES_SCREEN};
-  
+
+  FlipFlap(): win(Point(100,100),600,400,"FlipFlap"), running(true), screen(FlipFlap::SPLASH_SCREEN){}
+
   bool is_running();
   int current_screen();
   void done();
-  
+
+  void setup(int l);
+  void update();
+
   void show_splash();
   void show_levels();
   void show_game();
   void show_scores();
-  
+
   private:
   bool running;
   int screen;
 };
+#endif
