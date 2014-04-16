290267660ca2b2ec5d87fb21bf12d55f80cabbd9
Cooper95 <cooper.rehn@att.net>
2014-04-16 10:05:09 -0500

    Update Pancake.h

--- Pancake.h
+++ Pancake.h
@@ -2,14 +2,13 @@
 //CSCE 121-501
 //Pancake.h
 
-#ifndef FLIPFLAP_GUARD
-#define FLIPFLAP_GUARD
+#include "std_lib_facilities_4.h"
+#include "Graph.h"
 
-#include "FlipFlap.h"
+using namespace Graph_lib;
 
 struct Pancake : Rectangle {
   Pancake(int size, int position);
   int s;
   int p;
 };
-#endif
