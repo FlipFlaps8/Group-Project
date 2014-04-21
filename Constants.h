7af2d13524d45eaa02ef9e1caa0aa8e77c02bdc5
Cooper95 <cooper.rehn@att.net>
2014-04-21 09:16:28 -0500

    Create Constants.h

---
+++ Constants.h
@@ -0,0 +1,9 @@
+//Group 8
+//CSCE 121-501
+//Constants.h
+
+  static const int X_MAX = 599;
+  static const int Y_MAX = 399;
+  static const int X_CENTER = 299;
+  static const int Y_CENTER = 199;
+  static const int TABLE_TOP = 299;
