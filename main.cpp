c20cd6f664df073955ffe4e1dae0f119649ddee6
Cooper95 <cooper.rehn@att.net>
2014-04-22 18:36:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,22 +10,20 @@ FlipFlap engine;      //create the game engine
 int main()
   try{
     srand(time(0));
-    while(engine.is_running()){            //infinite loop
-      switch(engine.current_screen()){  //check game state
-        case FlipFlap::SPLASH_SCREEN:
-          engine.show_splash();         //show_x() functions actually do things
-          break;
-        case FlipFlap::LEVELS_SCREEN:
-          engine.show_levels();
-          break;
-        case FlipFlap::GAME_SCREEN:
-          engine.show_game();
-          break;
-        case FlipFlap::SCORES_SCREEN:
-          engine.show_scores();
-          break;
+    switch(engine.current_screen()){  //check game state
+      case FlipFlap::SPLASH_SCREEN:
+        engine.show_splash();         //show_x() functions actually do things
+        break;
+      case FlipFlap::LEVELS_SCREEN:
+        engine.show_levels();
+        break;
+      case FlipFlap::GAME_SCREEN:
+        engine.show_game();
+        break;
+      case FlipFlap::SCORES_SCREEN:
+        engine.show_scores();
+        break;
       }
-    }
     return gui_main();
   }
   catch(exception& e){
