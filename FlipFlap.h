a27d5fd7df9120f5b0a3f3725f75059609e9f34d
Cooper95 <cooper.rehn@att.net>
2014-04-22 16:18:23 -0500

    Update FlipFlap.h

--- FlipFlap.h
+++ FlipFlap.h
@@ -7,28 +7,25 @@
 
 #include "Simple_window.h"
 #include "Pancake.h"
+#include "GUI.h"
+#include <FL/Fl_Button.H>
 
-struct FlipFlap{
+using namespace Graph_lib;
 
-  static const int X_MAX = 599;
-  static const int Y_MAX = 399;
-  static const int X_CENTER = 299;
-  static const int Y_CENTER = 199;
-  static const int TABLE_TOP = 299;
-  Simple_window win;
+struct FlipFlap : Simple_window {
 
   enum{SPLASH_SCREEN, LEVELS_SCREEN, GAME_SCREEN, SCORES_SCREEN};
 
-  FlipFlap(): win(Point(100,100),600,400,"FlipFlap"), running(true), screen(FlipFlap::SPLASH_SCREEN){}
+  FlipFlap(): Simple_window(Point(100,100),600,400,"FlipFlap"), running(true), screen(FlipFlap::SPLASH_SCREEN){}
 
   bool is_running();
   int current_screen();
   void done();
 
   void setup(int l);
-  void cb_flip(Address button, Address window);
+  void update();
+  static void cb_flip(Address button, Address window);
   void flip(int p);
-
 
   void show_splash();
   void show_levels();
