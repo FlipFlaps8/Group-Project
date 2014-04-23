6fa27916d08efd29ffdd81c9c6b2ec0485c53176
drewmitchell94 <drew.mitchell94@yahoo.com>
2014-04-23 10:03:27 -0500

    Update Scores.cpp

--- Scores.cpp
+++ Scores.cpp
@@ -3,46 +3,51 @@
 //Scores.h
 #include "scores.h"
 #include "std_lib_facilities_4.h"
+#include <iostream>
 #define MAX_SCORES 5
 
 Scores::Scores(){
 	read_highscores();
-}
+};
 void Scores::read_highscores(){
-  ifstream iscore_file("HighScore.txt")
-  if(iscore_file.is_open()){
-	while(iscore_file.is_good()){
+  string filename = "HighScore.txt";
+  ifstream iscore_file(filename.c_str());
+  if(iscore_file){
+    cout<<"True?";
+	while(iscore_file.good()){
 		Player p;
 		iscore_file>>p.name;
 		iscore_file>>p.score;
 		scores.push_back(p);
 	}
-	iscore_file.close();
   }
-  
 }
 void Scores::write_highscores(){
-  ofstream oscore_file("HighScore.txt",ios::trunc);
-  for(int i = 0;i<scores.size();++i){
-	oscore_file<<scores[i].name<<' '<<scores[i].score;
+  scores.erase(scores.begin()+6,scores.end());
+  ofstream oscore_file("HighScore.txt",ios::out | ios::trunc);
+  for(int i = 0;i<scores.size()-1;++i){
+	oscore_file<<scores[i].name<<' '<<scores[i].score<<' ';
   }
-  oscore_file.close;
+  oscore_file.close();
 }
-void Score::game_score(string initials, int score){
+int Scores::game_score(string initials, int score){
 	int count = 0;
-	while(scores[count].score>score){
+	cout<<"Test3"<<endl;
+	while(count<5 && scores[count].score>score){
 		++count;
-		if(count=5)
-			return 0;
+		cout<<count<<endl;
 	}
+	cout<<count<<endl;
 	for(int i=4;i>count;--i){
 		scores[i]=scores[i-1];
+		cout<<scores[i].score;
 	}
 	Player p;
 	p.name=initials;
 	p.score=score;
 	scores[count]=p;
 }
-vector<Player> Scores::get_highscore(){
+vector<Player> Scores::get_highscores(){
 	return scores;
 }
+//end
