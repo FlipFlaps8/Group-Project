042b569c6c234b0bca1bdbb1089fe699c2633063
purpleflam3g <karlwhite@verizon.net>
2014-04-26 19:01:20 -0500

    Update Scores.cpp

--- Scores.cpp
+++ Scores.cpp
@@ -8,26 +8,26 @@
 
 Scores::Scores(){
 }
-void Scores::read_highscores(){
-  string filename = "HighScore.txt";
-  ifstream iscore_file(filename.c_str());
-  if(iscore_file){
+void Scores::read_highscores(){						// get high scores from file
+  string filename = "HighScore.txt";					//file name
+  ifstream iscore_file(filename.c_str());				//open file
+  if(iscore_file){							//if file opens
 	while(iscore_file.good()){
-		Player p;
-		iscore_file>>p.name;
-		iscore_file>>p.score;
-		scores.push_back(p);
+		Player p;						//make a new player
+		iscore_file>>p.name;					//get name from file
+		iscore_file>>p.score;					//get score from file
+		scores.push_back(p);					//add player to scores vector
 	}
   }
 }
-void Scores::write_highscores(){
-  scores.erase(scores.begin()+6,scores.end());
-  ofstream oscore_file("HighScore.txt",ios::out | ios::trunc);
-  for(int i = 0;i<scores.size()-1;++i){
+void Scores::write_highscores(){					//write high scores to file
+  scores.erase(scores.begin()+6,scores.end());				//delete any scores after five
+  ofstream oscore_file("HighScore.txt",ios::out | ios::trunc);		//clear old file
+  for(int i = 0;i<scores.size()-1;++i){					//get scores from scores vector
 	oscore_file<<scores[i].name<<' '<<scores[i].score<<' ';
   }
-  oscore_file.close();
-  scores.clear();
+  oscore_file.close();							//close file
+  scores.clear();							//clear scores vector
 }
 void Scores::game_score(string initials, int score){
 	int count = 0;
