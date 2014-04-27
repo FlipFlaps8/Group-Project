7f1cf46eddc4d24b9c526c7deeea683a209715ca
purpleflam3g <karlwhite@verizon.net>
2014-04-26 19:05:02 -0500

    Update Constants.h

--- Constants.h
+++ Constants.h
@@ -2,8 +2,8 @@
 //CSCE 121-501
 //Constants.h
 
-  static const int X_MAX = 599;
-  static const int Y_MAX = 399;
-  static const int X_CENTER = 299;
-  static const int Y_CENTER = 199;
-  static const int TABLE_TOP = 299;
+  static const int X_MAX = 599;                 //maximum x value of screen
+  static const int Y_MAX = 399;                 //maximum y value of screen
+  static const int X_CENTER = 299;              //x position of center of screen
+  static const int Y_CENTER = 199;              //y position of center of screen
+  static const int TABLE_TOP = 299;             //y value at base of pancake stack
