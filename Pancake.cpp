405df313629a8907b77ff077694909cd9b19c251
Cooper95 <cooper.rehn@att.net>
2014-04-28 09:19:03 -0500

    Update Pancake.cpp

--- Pancake.cpp
+++ Pancake.cpp
@@ -6,16 +6,16 @@
 
 int height = 20;
 
-int width1(int s1){
+int width1(int s1){	//calculate the width of a pancake from size s1
   return 40 + 40 * s1;
 }
 
-Point pPoint(int s, int p){
-        return Point(X_CENTER - width1(s)/2, TABLE_TOP - height * p);
+Point pPoint(int s, int p){	//calculate the top left point of a pancake with a given size and position
+	return Point(X_CENTER - width1(s)/2, TABLE_TOP - height * p);
 }
 
-Pancake::Pancake(int size, int position) : Rectangle(pPoint(size,position),width1(size),20), s(size), p(position){}
+Pancake::Pancake(int size, int position) : Rectangle(pPoint(size,position),width1(size),20), s(size), p(position){}	//construct a pancake
 
-int Pancake::get_size(){
-        return s;
+int Pancake::get_size(){	//get the size of a pancake
+	return s;
 }
